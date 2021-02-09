36e21111ffb6d204c882533bc53f4b31e90c4f2a
Durmuş PALA <71263028+durmus-pala@users.noreply.github.com>
2021-02-09 21:38:28 +0300

    I modified my code

--- letters_count.py
+++ letters_count.py
@@ -1,6 +1,5 @@
 sentence = input("Please Enter your sentence to analyze: ")
 result = dict()
 list_sentence = list(sentence)
-for i in list_sentence:
-    result.update({i : list_sentence.count(i)})
+result.update({i : list_sentence.count(i) for i in list_sentence})
 print(result)
